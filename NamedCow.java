6207d11d59765edeeed1d8163d593e163d333f32
BENJAMIN LEE <BELEE7@sfusd.edu>
2017-10-24 10:14:33 -0700

    Re-added all folders

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,19 @@
-class NamedCow
-{
-
+class namedCow extends Cow
+{     
+     protected String myName;     
+    
+     public namedCow(String type, String name, String sound)     {         
+         myType = type;         
+         mySound = sound;
+         myName = name;
+     }     
+     public namedCow()     {         
+         myName = "unknown";         
+          
+     }      
+     public String getName()
+   {
+   return myName;
+ }     
+ 
 }
